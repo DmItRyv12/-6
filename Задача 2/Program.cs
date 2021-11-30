80f05a656063f9958419e656dcf257c60ae67d79
Дмитрий Минаков <m1nak0vd@yandex.ru>
2021-11-30 21:20:33 +0300

    Добавьте файлы проекта.

---
+++ Задача 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Задача_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine(); 
+            string reverseStr = ""; 
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reverseStr += str[i]; 
+            }
+            if (str == reverseStr)
+                Console.WriteLine("Введенная строка является палиндромом.");
+            else
+                Console.WriteLine("Введенная строка не является палиндромом.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задача 6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Задача_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите предложение: ");
+            string input = Console.ReadLine();
+            string[] st = input.Split(new Char[] {' '});
+            int max = 0, index = 0;
+            for (int i = 0; i < st.Length; i++)
+            {
+                if (st[i].Length > max)
+                {
+                    max = st[i].Length;
+                    index = i;
+                }
+            }
+            Console.Write("Самое длинное слово в предложении : {0}", st[index]);
+            Console.ReadLine();
+        }
+    }
+}
